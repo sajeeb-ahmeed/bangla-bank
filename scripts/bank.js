8794dfea4aaa74df90228ca6b16c5040466de60d
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-02-10 16:45:21 +0600

    deposit scripts add

--- scripts/bank.js
+++ scripts/bank.js
@@ -7,5 +7,20 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     const NewDepositAmount = parseFloat(NewDepositAmountText);
     // console.log(depositAmount);
 
+    //new deposit total update
+    const depositCurrent = document.getElementById('deposit-current');
+    const priviousDepositAmountText = depositCurrent.innerText;
+    const priviousDepositAmount = parseFloat(priviousDepositAmountText);
+    const totalDepositAmount = priviousDepositAmount + NewDepositAmount;
+    depositCurrent.innerText = totalDepositAmount;
 
+    // balance update 
+    const balance = document.getElementById('balance-total');
+    const balanceTotalText = balance.innerText;
+    const balanceTotal = parseFloat(balanceTotalText);
+    const newTotalBalance = balanceTotal + NewDepositAmount;
+    balance.innerText = newTotalBalance;
+
+    // clear input 
+    depositInput.value = '';
 })
